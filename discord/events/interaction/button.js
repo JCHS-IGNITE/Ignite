e3d0c92f6b70da19c00e45bb025e6abf4e74f460
Ioloolo <admin@sondaehyeon.studio>
2022-07-08 22:37:19 +0900

    update

--- discord/events/interaction/button.js
+++ discord/events/interaction/button.js
@@ -1,13 +1,11 @@
-const manualVerifyFunction = require('./button/manualVerifyFunction');
+const manualVerify = require('./button/manualVerify');
 const recheckDmPermission = require('./button/recheckDmPermission');
-const predictionConfirm = require('./button/predictionConfirm');
-const predictionExit = require('./button/predictionExit');
+const predictionStart = require('./button/predictionStart');
 
 module.exports = async (client, interaction) => {
   if (interaction.isButton()) {
-    await manualVerifyFunction(client, interaction);
+    await manualVerify(client, interaction);
     await recheckDmPermission(client, interaction);
-    await predictionConfirm(client, interaction);
-    await predictionExit(client, interaction);
+    await predictionStart(client, interaction);
   }
 };
